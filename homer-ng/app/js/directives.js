fd14f18bf447e86c94c60993b79f651e2a828740
Gary Bortosky <gary@bortosky.com>
2013-11-23 20:20:37 -0600

    rudimentary animation

--- homer-ng/app/js/directives.js
+++ homer-ng/app/js/directives.js
@@ -1,4 +1,5 @@
 /// <reference path="../lib/typings/google/angular-1.0.d.ts" />
+/// <reference path="../lib/typings/jquery/jquery.d.ts" />
 /// <reference path="homer.ts" />
 var HomerWeb;
 (function (HomerWeb) {
@@ -12,7 +13,20 @@ var HomerWeb;
         var directiveDefinitionObject = {
             templateUrl: 'partials/address-detail.html',
             scope: {
-                address: '=address'
+                address: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                $scope.onChange = function (loca) {
+                    if (!!loca) {
+                        console.log('changed', loca, $element);
+                        $element.removeClass('homerfun').addClass('homerfun');
+                    }
+                };
+            },
+            link: function (scope, elem, a) {
+                scope.$watch('address', function (newVal) {
+                    return scope.onChange(newVal);
+                });
             }
         };
         return directiveDefinitionObject;
--- homer-ng/app/js/directives.ts
+++ homer-ng/app/js/directives.ts
@@ -1,11 +1,14 @@
 /// <reference path="../lib/typings/google/angular-1.0.d.ts" />
+/// <reference path="../lib/typings/jquery/jquery.d.ts" />
 /// <reference path="homer.ts" />
 
 module HomerWeb {
 
     'use strict';
 
-    interface Scope extends ng.IScope, Homer.Loca { }
+    interface Scope extends ng.IScope, Homer.Loca {
+        onChange(loca: Homer.Loca): void;
+    }
 
     interface ILinkAttributes extends ng.IAttributes {
         homerLocaDirective: string;
@@ -17,8 +20,19 @@ module HomerWeb {
         var directiveDefinitionObject: ng.IDirective = {
             templateUrl: 'partials/address-detail.html',
             scope: {
-                address: '=address'
+                address: '='
             },
+            controller: ($scope: Scope, $element: JQuery, $attrs: ng.IAttributes) => {
+                $scope.onChange = (loca) => {
+                    if (!!loca) {
+                        console.log('changed', loca, $element);
+                        $element.removeClass('homerfun').addClass('homerfun');
+                    }
+                };
+            },
+            link: (scope: Scope, elem: JQuery, a: ng.IAttributes) => {
+                scope.$watch('address', (newVal) => scope.onChange(newVal));
+            }
         };
         return directiveDefinitionObject;
     });

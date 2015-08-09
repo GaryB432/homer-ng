38fec146a327979a185587fa02a2db0ec68ab8b9
Gary Bortosky <gary@bortosky.com>
2015-08-08 22:38:50 -0500

    update tsc

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 
-gulp.task('default', function() {
-  // place code for your default task here
+
+gulp.task('scripts', function () {
+    return gulp.src([
+        'source/bower_components/angular/angular.js',
+        'source/bower_components/angular-touch/angular-touch.js',
+        'source/bower_components/angular-animate/angular-animate.js',
+        'source/bower_components/angular-resource/angular-resource.js',
+        'source/bower_components/angular-route/angular-route.js',
+        'source/geo/geo.js'
+    ])
+        .pipe(concat('lib.js'))
+        .pipe(gulp.dest('./app/js/'));
 });
+ 
+gulp.task('default', ['scripts']);

a9f1fa8c2dd9a571fc14563be49131cbf82d67dc
Gary Bortosky <gary@bortosky.com>
2015-08-13 02:00:59 -0500

    directive fixup

--- source/js/directives/location.ts
+++ source/js/directives/location.ts
@@ -1,22 +1,9 @@
 namespace HomerWeb {
-    interface Scope extends ng.IScope, ILocation {
-        name: string;
-    }
-
-    interface ILinkAttributes extends ng.IAttributes {
-        address: string;
-        homerLocaDirective: string;
-    }
-
     App.directive('homerLocation', function factory($animate: ng.IAnimateService) {
         let directiveDefinitionObject: ng.IDirective = {
             templateUrl: 'templates/directives/location.html',
             scope: {
-                address: '=',
-                latLon: '&'
-            },
-            controller: ($scope: Scope, $element: JQuery, $attrs: ILinkAttributes) => {
-                $scope.name = $attrs.address;
+                location: '='
             }
         };
         return directiveDefinitionObject;
